7a99a323a9fc01b2072c12b7fa6149f71ece9128
Franek Wysocki <wysocki.f.00@gmail.com>
2021-11-17 19:03:56 +0100

    Add navbar with title

---
+++ src/frontend/src/containers/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import {Navbar} from "reactstrap";
+
+function App() {
+    return (
+        <Navbar color="dark" dark container="fluid">
+            <div className="text-center col-12">
+                <h3 className="text-light">Skin disease detection app</h3>
+            </div>
+        </Navbar>
+    );
+}
+
+export default App;
